cc7d4ec93b055a7ddec02a6850186e1e23f7781b
EmpiricistSkeptic <131171004+EmpiricistSkeptic@users.noreply.github.com>
2024-12-23 15:51:35 +0000

    some changes

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('poems/', views.poems, name='poems'),
-     path('gifts/', views.gifts, name='gifts'), 
+    path('gifts/', views.gifts, name='gifts'), 
     path('letters/', views.letters, name='letters'),
 ]
